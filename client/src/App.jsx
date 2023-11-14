c552dbdd02d7cc562e61344ead6900107cf64b96
Konstantin <kzaraliev@gmail.com>
2023-11-14 23:44:22 +0200

    Start from zero

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import Footer from "./components/Footer/Footer.jsx";
+import Navigation from "./components/Navigation/Navigation.jsx";
+import Home from "./components/Home/Home.jsx";
 
+import { Routes, Route } from "react-router-dom";
+
+function App() {
   return (
     <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="*" element={<NotFount />} /> */}
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
