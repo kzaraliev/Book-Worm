d7d24dba3af11ef4b2373f3fb4fee78ff06a788b
Konstantin <kzaraliev@gmail.com>
2023-11-16 23:12:43 +0200

    Fix "more books" button

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,18 +2,19 @@ import styles from "./Home.module.css";
 import WelcomeHome from "./WelcomeHome/WelcomeHome.jsx";
 import BookOfTheDayList from "./BookOfTheDay/BookOfTheDayList.jsx";
 import { FaBook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <WelcomeHome />
       <BookOfTheDayList />
-      <button className={styles.moreBooks}>
+      <Link to={"/books"} type="button" className={styles.moreBooks}>
         <div className={styles.textContainer}>
-          <span >More Books</span>
+          <span>More Books</span>
           <FaBook className={styles.moreBooksIcon} />
         </div>
-      </button>
+      </Link>
     </div>
   );
 }
