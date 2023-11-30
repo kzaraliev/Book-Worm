16b8fdf4a850e55c9238987010ffdd68d951c389
Konstantin <kzaraliev@gmail.com>
2023-12-01 00:40:04 +0200

    add validation for creating book

--- client/src/components/CreateBook/CreateBook.jsx
+++ client/src/components/CreateBook/CreateBook.jsx
@@ -1,97 +1,173 @@
+import { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
-
-import Path from "../../paths";
-import styles from "../../styles/FormStyles.module.css";
-import * as bookService from "../../services/bookService";
-
 import Form from "react-bootstrap/Form";
+import { useFormik } from "formik";
+
+import { CreateFormKeys } from "../../utils/constants";
+import Path from "../../paths";
+import { createBookValidation } from "./createBookValidation";
+import * as bookService from "../../services/bookService";
+import styles from "../../styles/FormStyles.module.css";
+import AuthContext from '../../context/authContext';
+
+const initialValues = {
+  [CreateFormKeys.Title]: "",
+  [CreateFormKeys.Author]: "",
+  [CreateFormKeys.Year]: "",
+  [CreateFormKeys.Genre]: "",
+  [CreateFormKeys.Description]: "",
+  [CreateFormKeys.ImageUrl]: "",
+};
 
 export default function CreateBook() {
   const navigate = useNavigate();
+  const { logoutHandler } = useContext(AuthContext);
 
-  const createBookSubmitHandler = async (e) => {
-    e.preventDefault();
+  const {
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
+    handleBlur,
+    isSubmitting,
+    setFieldValue,
+    touched,
+    resetForm,
+  } = useFormik({
+    initialValues,
+    validationSchema: createBookValidation,
+    onSubmit,
+  });
 
-    const bookData = Object.fromEntries(new FormData(e.currentTarget));
-
+  async function onSubmit() {
     try {
-      await bookService.create(bookData);
+      await bookService.create(values);
 
       navigate(Path.Books);
-    } catch (err) {
-      // Error notification
-      console.log(err);
+    } catch (error) {
+      if (error.code === 401) {
+        resetForm();
+
+        logoutHandler();
+
+        navigate(Path.Login);
+      }
     }
-  };
+  }
+
   return (
     <div className={styles.containerForm}>
-      <Form className={styles.form} onSubmit={createBookSubmitHandler}>
+      <Form onSubmit={handleSubmit} className={styles.form} >
         <h1 className={styles.title}>Create Book</h1>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="title">Book Title</Form.Label>
+          {errors[CreateFormKeys.Title] && touched[CreateFormKeys.Title] && (
+            <p className={styles.invalid}>{errors[CreateFormKeys.Title]}</p>
+          )}
+          <Form.Label htmlFor={CreateFormKeys.Title}>Book Title</Form.Label>
           <Form.Control
             type="text"
-            id="title"
-            name="title"
+            id={CreateFormKeys.Title}
+            name={CreateFormKeys.Title}
             placeholder="Enter book title"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values[CreateFormKeys.Title]}
           />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="author">Author</Form.Label>
-          <Form.Control
-            type="author"
-            id="author"
-            name="author"
-            placeholder="Enter author"
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="year">Published on</Form.Label>
+          {errors[CreateFormKeys.Author] && touched[CreateFormKeys.Author] && (
+            <p className={styles.invalid}>{errors[CreateFormKeys.Author]}</p>
+          )}
+          <Form.Label htmlFor={CreateFormKeys.Author}>Author</Form.Label>
           <Form.Control
             type="text"
-            id="year"
-            name="year"
-            placeholder="Enter year of publication"
+            id={CreateFormKeys.Author}
+            name={CreateFormKeys.Author}
+            placeholder="Enter author"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values[CreateFormKeys.Author]}
           />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="genre">Genre</Form.Label>
+          {errors[CreateFormKeys.Year] && touched[CreateFormKeys.Year] && (
+            <p className={styles.invalid}>{errors[CreateFormKeys.Year]}</p>
+          )}
+          <Form.Label htmlFor={CreateFormKeys.Year}>Published on</Form.Label>
           <Form.Control
-            type="genre"
-            id="genre"
-            name="genre"
-            placeholder="Enter genre"
+            type="number"
+            id={CreateFormKeys.Year}
+            name={CreateFormKeys.Year}
+            placeholder="Enter year of publication"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values[CreateFormKeys.Year]}
           />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="description">Description</Form.Label>
+          {errors[CreateFormKeys.Genre] && touched[CreateFormKeys.Genre] && (
+            <p className={styles.invalid}>{errors[CreateFormKeys.Genre]}</p>
+          )}
+          <Form.Label htmlFor={CreateFormKeys.Genre}>Genre</Form.Label>
+          <Form.Control
+            type="text"
+            id={CreateFormKeys.Genre}
+            name={CreateFormKeys.Genre}
+            placeholder="Enter genre"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values[CreateFormKeys.Genre]}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          {errors[CreateFormKeys.Description] &&
+            touched[CreateFormKeys.Description] && (
+              <p className={styles.invalid}>
+                {errors[CreateFormKeys.Description]}
+              </p>
+            )}
+          <Form.Label htmlFor={CreateFormKeys.Description}>
+            Description
+          </Form.Label>
           <Form.Control
             className={styles.textArea}
             as="textarea"
             rows={3}
-            type="description"
-            id="description"
-            name="description"
+            type="text"
+            id={CreateFormKeys.Description}
+            name={CreateFormKeys.Description}
             placeholder="Enter description"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values[CreateFormKeys.Description]}
           />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="imageUrl">Book image</Form.Label>
+          {errors[CreateFormKeys.ImageUrl] &&
+            touched[CreateFormKeys.ImageUrl] && (
+              <p className={styles.invalid}>
+                {errors[CreateFormKeys.ImageUrl]}
+              </p>
+            )}
+          <Form.Label htmlFor={CreateFormKeys.ImageUrl}>Book image</Form.Label>
           <Form.Control
             type="text"
-            id="imageUrl"
-            name="imageUrl"
+            id={CreateFormKeys.ImageUrl}
+            name={CreateFormKeys.ImageUrl}
             placeholder="Enter image URL"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values[CreateFormKeys.ImageUrl]}
           />
         </Form.Group>
 
-        <button className={styles.submitButton} variant="primary" type="submit">
+        <button className={styles.submitButton} variant="primary" type="submit" value="Create Post">
           Create
         </button>
       </Form>
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,12 +1,21 @@
 export const LoginFormKeys = {
-    Email: 'email',
-    Password: 'password',
+  Email: "email",
+  Password: "password",
 };
 
 export const RegisterFormKeys = {
-    Email: 'email',
-    Username: 'username',
-    ImgURL: 'img',
-    Password: 'password',
-    ConfirmPassword: 'confirmPassword',
+  Email: "email",
+  Username: "username",
+  ImgURL: "img",
+  Password: "password",
+  ConfirmPassword: "confirmPassword",
+};
+
+export const CreateFormKeys = {
+  Title: "title",
+  Author: "author",
+  Year: "year",
+  Genre: "genre",
+  Description: "description",
+  ImageUrl: "imageUrl",
 };
