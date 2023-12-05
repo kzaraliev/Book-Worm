f2019ed74bf0e179f0529750484ba19e7c1c237f
Konstantin <kzaraliev@gmail.com>
2023-12-05 19:34:01 +0200

    Add order button for the books

--- client/src/components/Books/BooksList.jsx
+++ client/src/components/Books/BooksList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { LiaSadTear } from "react-icons/lia";
+import Dropdown from "react-bootstrap/Dropdown";
 
 import * as bookService from "../../services/bookService.js";
 import Book from "./Book.jsx";
@@ -12,6 +13,7 @@ export default function Books() {
   const [books, setBooks] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [booksPerPage] = useState(6);
+  const [sortText, setSortText] = useState("");
 
   useEffect(() => {
     bookService
@@ -29,15 +31,60 @@ export default function Books() {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  function selectOrderHandler(eventKey) {
+    setBooks(
+      [...books].sort((a, b) => {
+        if (
+          typeof a[eventKey] === "number" &&
+          typeof b[eventKey] === "number"
+        ) {
+          return a[eventKey] - b[eventKey];
+        } else if (
+          typeof a[eventKey] === "string" &&
+          typeof b[eventKey] === "string"
+        ) {
+          return a[eventKey].localeCompare(b[eventKey]);
+        }
+      })
+    );
+
+    if (eventKey === "year") {
+      setSortText("Year of publication");
+    } else {
+      setSortText(eventKey.charAt(0).toUpperCase() + eventKey.slice(1));
+    }
+  }
+
   return (
     <>
       {books.length === 0 ? (
         <div className={styles.empty}>
-          <h3 className={styles.emptyText}>No books yet <LiaSadTear /></h3>
-          <Link className={styles.link} to={Path.CreateBook}>*Add your favorites</Link>
+          <h3 className={styles.emptyText}>
+            No books yet <LiaSadTear />
+          </h3>
+          <Link className={styles.link} to={Path.CreateBook}>
+            *Add your favorites
+          </Link>
         </div>
       ) : (
         <div className={styles.books}>
+          <div className={styles.header}>
+            <h1 className={styles.title}>All books:</h1>
+            <Dropdown className={styles.dropdown} onSelect={selectOrderHandler}>
+              <Dropdown.Toggle id="dropdown-basic">
+                {sortText !== "" ? `Order by: ${sortText}` : "Order by"}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey="title">Title</Dropdown.Item>
+                <Dropdown.Item eventKey="author">Author</Dropdown.Item>
+                <Dropdown.Item eventKey="year">
+                  Year of publication
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="genre">Genre</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
           <div className={styles.grid}>
             {currentBooks.map((book) => (
               <Book
